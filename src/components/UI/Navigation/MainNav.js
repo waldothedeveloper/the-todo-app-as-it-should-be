9d23c87787d40c6c51090a3aa51dc0bd5ea1700d
Waldo Lavaut <w86compositor@gmail.com>
2018-10-09 15:34:09 -0400

    Added a eslint file and fix the padding on the search input

--- src/components/UI/Navigation/MainNav.js
+++ src/components/UI/Navigation/MainNav.js
@@ -5,15 +5,45 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-const styles = {
+const styles = (theme) => ({
 	root: {
 		flexGrow: 1
 	},
 	grow: {
 		flexGrow: 1
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		},
+		marginRight: theme.spacing.unit * 2,
+		marginLeft: 0,
+		width: 100,
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit * 3,
+			width: 'auto'
+		}
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%'
+	},
+	inputInput: {
+		padding: theme.spacing.unit,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200
+		}
 	}
-};
+});
 
 function ButtonAppBar(props) {
 	const { classes } = props;
@@ -22,8 +52,19 @@ function ButtonAppBar(props) {
 			<AppBar position="static">
 				<Toolbar>
 					<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+						{/*This is the nice hamburger */}
 						<MenuIcon />
 					</IconButton>
+					{/*Following stuff below the menu icon */}
+					<div className={classes.search}>
+						<InputBase
+							placeholder="Search…"
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput
+							}}
+						/>
+					</div>
 				</Toolbar>
 			</AppBar>
 		</div>
